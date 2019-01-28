abe6f0f63b4e65b132398f2fc65c10f31f6ffd42
jpabon <jpabon@madisoncollege.edu>
2019-01-27 23:11:16 -0600

    Adding Unit 1 Lab 2

---
+++ src/java112/demos/HelloWorld.java
@@ -0,0 +1,18 @@
+package java112.demos;
+
+/**
+* A first class to verify the environment is working
+* It is the simplest Hello World class
+*
+* @author jpabon
+*/
+public class HelloWorld {
+
+    /** Write oyt a simple message
+     * @param args command line argument
+     */
+    public static void main(String[] args) {
+        System.out.println("Woohoo! It's working!");
+    }
+
+}
