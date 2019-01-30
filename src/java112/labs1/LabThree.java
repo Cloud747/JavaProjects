205cfb7729f52026e2d5cd50c83d055b81791a85
jpabon <jpabon@madisoncollege.edu>
2019-01-29 22:16:39 -0600

    All test passing for lab three

--- src/java112/labs1/LabThree.java
+++ src/java112/labs1/LabThree.java
@@ -3,9 +3,15 @@ package java112.labs1;
 public class LabThree {
 
     public static void main(String[] args) {
+        LabThree lab = new LabThree();
 
-        
+        if (args.length != 1) {
+            System.out.println("Please enter one argument on the command line");
+        } else {
+            lab.run("some string");
+        }
     }
+
     public void run(String input) {
         System.out.println("input: test");
     }
