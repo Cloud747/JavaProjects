290a9ecba6045df531c2363719ebacec1d2f24bb
jpabon <jpabon@madisoncollege.edu>
2019-02-11 15:33:01 -0600

    LabFive completed with all new added code

--- src/java112/labs1/LabFive.java
+++ src/java112/labs1/LabFive.java
@@ -15,14 +15,32 @@ public class LabFive {
     public static void main(String[] args) {
         LabFive lab = new LabFive();
 
-        //checking if there are two command line arguments
+        // checking if there are two command line arguments
+        // added the second parameter to the run method
         if (args.length != 2) {
             System.out.println("Please enter two arguments on the command line, a file name and a message");
         } else {
             lab.run(args[0], args[1]);
         }
     }
+    /**
+    * Creating the run method that will pass in the inputName and Message
+    * @param inputName
+    * @param message
+    */
     public void run(String inputName, String message) {
-
+        PrintWriter outputWriter = null;
+        try {
+            outputWriter = new PrintWriter(new BufferedWriter(new FileWriter(inputName)));
+            outputWriter.println(message);
+        } catch (IOException inputOutputException) {
+            inputOutputException.printStackTrace();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        } finally {
+            if (outputWriter != null) {
+                outputWriter.close();
+            }
+        }
     }
 }
