4c29534f848d2af886e1bc819859817e2d654e90
jpabon <jpabon@madisoncollege.edu>
2019-02-11 15:59:07 -0600

    Adding more structure to LabSix

--- src/java112/labs1/LabSix.java
+++ src/java112/labs1/LabSix.java
@@ -7,6 +7,17 @@ import java.io.*;
 *
 */
 public class LabSix {
+    public static void main(String[] args) {
+        LabSix lab = new LabSix();
 
+        // checking for two command line arguments
+        // added the second parameter to run the method
+        if (args.length != 2) {
+            System.out.println("please enter two arguments on the command line, "
+            + " an input file name and an output file name");
+        } else {
+            lab.run(args[0], args[1]);
+        }
+    }
 
 }
