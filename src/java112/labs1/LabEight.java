95598f16c2216af4a55ad18bc7f986fa29d6f46b
jpabon <jpabon@madisoncollege.edu>
2019-02-18 11:16:44 -0600

    Added the the run method along with the sorted set of strings

--- src/java112/labs1/LabEight.java
+++ src/java112/labs1/LabEight.java
@@ -2,6 +2,7 @@ package java112.labs1;
 
 
 public class LabEight {
+    Set<String> set;
 
     public static void main(String[] args) {
         LabEight lab = new LabEight();
@@ -9,6 +10,15 @@ public class LabEight {
         if (args.length != 1) {
             System.out.println("Please enter one argument on the command line, "
             + "an input file name and an output file name");
+        } else {
+            lab.run(args[0]);
         }
+    }
+    /**
+    *
+    *
+    */
+    public void run(String outputFile)  {
+
     }
 }
