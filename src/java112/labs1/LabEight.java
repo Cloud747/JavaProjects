4d0dea8788f5630cd56e81e395c21e281e1a67ff
jpabon <jpabon@madisoncollege.edu>
2019-02-18 10:48:35 -0600

    Added LabEight to the mix

---
+++ src/java112/labs1/LabEight.java
@@ -0,0 +1,14 @@
+package java112.labs1;
+
+
+public class LabEight {
+
+    public static void main(String[] args) {
+        LabEight lab = new LabEight();
+
+        if (args.length != 1) {
+            System.out.println("Please enter one argument on the command line, "
+            + "an input file name and an output file name");
+        }
+    }
+}
