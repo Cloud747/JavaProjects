1fdbdfd850de952222af81566b10496afe5a34f1
jpabon <jpabon@madisoncollege.edu>
2019-02-18 15:25:37 -0600

    created two classes to start off the project 1

---
+++ src/java112/analyzer/Driver.java
@@ -0,0 +1,11 @@
+package java112.analyzer;
+
+/**
+*
+*
+*
+*/
+public class Driver {
+
+
+}
---
+++ src/java112/analyzer/FileAnalysis.java
@@ -0,0 +1,13 @@
+package java112.analyzer;
+
+/**
+*
+* @author jpabon
+*
+*/
+public class FileAnalysis {
+    
+    public static void main (String[] args) {
+
+    }
+}
