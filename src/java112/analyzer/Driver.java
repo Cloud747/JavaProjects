8381097f3220e99a1498f79c04a7dc27553c00cb
jpabon <jpabon@madisoncollege.edu>
2019-02-18 16:18:35 -0600

    adding more code to the driver and fileanalysis classes

--- src/java112/analyzer/Driver.java
+++ src/java112/analyzer/Driver.java
@@ -6,6 +6,8 @@ package java112.analyzer;
 *
 */
 public class Driver {
-
-
+    public static void main(String[] args) {
+        FileAnalysis startFile = new FileAnalysis();
+        startFile.analyze(arguments);
+    }
 }
--- src/java112/analyzer/FileAnalysis.java
+++ src/java112/analyzer/FileAnalysis.java
@@ -1,13 +1,25 @@
 package java112.analyzer;
-
 /**
 *
 * @author jpabon
 *
 */
 public class FileAnalysis {
-    
-    public static void main (String[] args) {
+
+    private static final int VALID_ARGUMENT_NUMBER_COUNT = 1;
+    private SummaryAnalyzer summaryAnalyzer;
+    private DistinctAnalyzer distinctAnalyzer;
+
+    /**
+    *
+    * @param arguments
+    *
+    */
+    public void analyze(String[] arguments){
+        if (arguments.length != VALID_ARGUMENT_NUMBER_COUNT) {
+            System.out.println("Please enter the right input");
+            return;
+        }
 
     }
 }
