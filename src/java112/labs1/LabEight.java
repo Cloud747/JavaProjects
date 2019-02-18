812019ffce0fa967275ee896425b2fe2e789c507
jpabon <jpabon@madisoncollege.edu>
2019-02-18 11:21:36 -0600

    Added TreeSet and ten elements to the list

--- src/java112/labs1/LabEight.java
+++ src/java112/labs1/LabEight.java
@@ -1,6 +1,10 @@
 package java112.labs1;
 
-
+/**
+*
+* @author jpabon
+*
+*/
 public class LabEight {
     Set<String> set;
 
@@ -16,9 +20,23 @@ public class LabEight {
     }
     /**
     *
+    * @param String The outputFile
     *
     */
     public void run(String outputFile)  {
+        set = new TreeSet<String>();
 
+        set.add("one");
+        set.add("one");
+        set.add("five");
+        set.add("two");
+        set.add("four");
+        set.add("two");
+        set.add("three");
+        set.add("three");
+        set.add("four");
+        set.add("three");
+
+        writeSetToOutputFile(outputFile);
     }
 }
