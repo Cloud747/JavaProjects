beb78da12f827939c10413441b593f333623328d
jpabon <jpabon@madisoncollege.edu>
2019-02-19 14:49:31 -0600

    passed all the driver tests

--- src/java112/analyzer/Driver.java
+++ src/java112/analyzer/Driver.java
@@ -1,12 +1,14 @@
 package java112.analyzer;
 
+
 /**
 *
 * @author jpabon
 *
 */
+
 public class Driver {
-    public static void main(String[] args) {
+    public static void main(String[] arguments) {
         FileAnalysis startFile = new FileAnalysis();
         startFile.analyze(arguments);
     }
