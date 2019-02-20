c63c15240bfa1aefe5256381ba1462a3b07f7eee
jpabon <jpabon@madisoncollege.edu>
2019-02-19 22:41:27 -0600

    added the openInputFile and created a method that will open the input file and loop through all the lines

--- src/java112/analyzer/FileAnalysis.java
+++ src/java112/analyzer/FileAnalysis.java
@@ -26,14 +26,43 @@ public class FileAnalysis {
             return;
         }
         /**
-        * @param
+        * @param args
         *
         */
-        public void openInputFile(String inputFilePath) {
+        void openInputFile(String inputFilePath) {
 
         }
-        public void writeOutputFiles() {
+        void writeOutputFiles() {
             this.generateOutputFile();
+        }
+        public void run(String testFile) {
+            BufferedReader inputReader = null;
+            try {
+                inputReader = new BufferedReader(new FileReader(testFile));
+                // code to process the input file
+                // declare a string variable to hold the current line
+                String line = null;
+                while (inputReader.ready()) {
+                    //reading through each line
+                    line = inputReader.readLine();
+                    //outputting the text
+                    System.out.println(line);
+                }
+            } catch (FileNotFoundException fileNotFound) {
+                fileNotFound.printStackTrace();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            } finally {
+                try {
+                    if (inputReader != null) {
+                        inputReader.close();
+                    }
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
         }
     }
 }
