ef8608e1fa43c638c77d20f821c7cbfaf9155160
jpabon <jpabon@madisoncollege.edu>
2019-04-17 14:15:26 -0500

    added the javabean and entered more methods into the servlet for project 3

---
+++ src/java112/project3/HttpRequestData.java
@@ -0,0 +1,53 @@
+package java112.project3;
+
+
+/**
+*
+*
+* @author jpabon 
+*/
+
+public class HttpRequestData {
+
+
+
+    private String remoteComputerName;
+    private 
+    private 
+
+
+    The remote computer making the request
+    The address of the remote computer making the request
+    The HTTP method of the request
+    The request URI
+    The request URL
+    The protocol of the request
+    The server name
+    The server port number
+    The current Locale of the server
+    The query string
+    The value of a query parameter with the name of “queryParameter”
+    The value of the request header with the name of “User-Agent”
+
+
+    /**
+     *  Gets the Data attribute of the BeanOne object
+     *
+     *@return    The Data value
+     */
+    public String getData1() {
+        //return data;
+        return "Some new data thing!";
+    }
+
+    /**
+     *  Sets the Data attribute of the BeanOne object
+     *
+     *@param  data  The new Data value
+     */
+    public void setData(String data) {
+        //this.data = data;
+    }
+}
+
+}
